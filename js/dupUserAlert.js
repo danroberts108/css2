cbf639173f5f5748943f73bc5207d124ed026f87
danroberts108 <d.roberts15@edu.salford.ac.uk>
2022-03-19 19:04:55 +0000

    Removed debug log statement

--- js/dupUserAlert.js
+++ js/dupUserAlert.js
@@ -4,7 +4,6 @@ let username = document.getElementById('usernameInput');
 function removeDupAlert() {
     alert.remove();
     username.classList.remove('border-danger');
-    console.log("removed class");
 }
 
 document.getElementById('usernameInput').addEventListener('focusout', removeDupAlert, false);
